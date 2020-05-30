3babe358e5cafcaf230d3462e0105d4797eba45d
Ritul Singh <53478620+ritulsingh@users.noreply.github.com>
2020-05-30 19:13:02 +0530

    Day 1 Solution

--- Day 1: Data Types/Solution.java
+++ Day 1: Data Types/Solution.java
@@ -8,26 +8,22 @@ public class Solution {
 
         Scanner scan = new Scanner(System.in);
 
-        /* Declare second integer, double, and String variables. */
+       
         int i2;
         double d2;
         String s2;
 
-        /* Read and save an integer, double, and String to your variables.*/
         i2 = scan.nextInt();
         d2 = scan.nextDouble();
 
         scan.nextLine();
         s2 = scan.nextLine();
 
-        /* Print the sum of both integer variables on a new line. */
+       
         System.out.println(i + i2);
 
-        /* Print the sum of the double variables on a new line. */
         System.out.println(d + d2);
-
-        /* Concatenate and print the String variables on a new line;
-            the 's' variable above should be printed first. */
+      
         System.out.println(s.concat(s2));
 
         scan.close();
