6ccd30bc44ef72ad6867dd566b005fccf8e11ffd
Ritul Singh <53478620+ritulsingh@users.noreply.github.com>
2020-06-08 14:38:43 +0530

    Day 27 Solution

--- Day 27 Testing/Solution.py
+++ Day 27 Testing/Solution.py
@@ -1,11 +1,59 @@
-print(5)
-print("4 3")
-print("0 -3 4 2")
-print("5 2")
-print("0 -3 4 2 2")
-print("3 3")
-print("0 -3 4")
-print("7 2")
-print("0 -3 1 1 1 1 1")
-print("6 3")
-print("0 -3 4 2 1 1")
+def minimum_index(seq):
+    if len(seq) == 0:
+        raise ValueError("Cannot get the minimum value index from an empty sequence")
+    min_idx = 0
+    for i in range(1, len(seq)):
+        if seq[i] < seq[min_idx]:
+            min_idx = i
+    return min_idx
+
+class TestDataEmptyArray(object):
+    def get_array():
+        return list()
+class TestDataUniqueValues(object):
+    def get_array():
+        return [7,9,1,2,-7,10,22]
+    def get_expected_result():
+        return 4
+class TestDataExactlyTwoDifferentMinimums(object):
+    def get_array():
+        return [7,9,1,2,-7,10,22,-7]
+    def get_expected_result():
+        return 4
+
+def TestWithEmptyArray():
+    try:
+        seq = TestDataEmptyArray.get_array()
+        result = minimum_index(seq)
+    except ValueError as e:
+        pass
+    else:
+        assert False
+
+
+def TestWithUniqueValues():
+    seq = TestDataUniqueValues.get_array()
+    assert len(seq) >= 2
+
+    assert len(list(set(seq))) == len(seq)
+
+    expected_result = TestDataUniqueValues.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+
+def TestiWithExactyTwoDifferentMinimums():
+    seq = TestDataExactlyTwoDifferentMinimums.get_array()
+    assert len(seq) >= 2
+    tmp = sorted(seq)
+    assert tmp[0] == tmp[1] and (len(tmp) == 2 or tmp[1] < tmp[2])
+
+    expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+TestWithEmptyArray()
+TestWithUniqueValues()
+TestiWithExactyTwoDifferentMinimums()
+print("OK")
+
